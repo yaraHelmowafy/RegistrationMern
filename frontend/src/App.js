377fc1e4f116685194dbea10a27a8035770519e8
yaraHelmowafy <https://github.com/yaraHelmowafy>
2024-07-15 00:14:07 +0300

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+           
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
